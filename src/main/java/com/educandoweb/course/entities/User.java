99dd15c95e0e7da34b02ac2e3aa39fc45e5043c6
Hamilton do Carmo <zegaiver7@hotmail.com>
2023-08-18 07:36:40 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -3,10 +3,20 @@ package com.educandoweb.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity                      //Anotation que fala pra converter a entidade para tabela
+@Table(name = "tb_user")     //Anotation que fala pra mudar o nome da tabela para tb_user
 public class User implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id                      //Anotation que fala que o campo abaixo é chave primária
+	@GeneratedValue(strategy = GenerationType.IDENTITY)  //Anotation que fala que a chave é autoinc
 	private Long id;
 	private String name;
 	private String email;
